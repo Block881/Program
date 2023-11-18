1518bee2ec3d0e2b59cb6a3313b7490717589034
Block881 <150650588+Block881@users.noreply.github.com>
2023-11-18 18:18:58 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -135,7 +135,7 @@ namespace ConsoleApp1
             Zadanie8();
         }
 
-
+//https://github.com/blockin90/Programming_PIB/blob/master/%D0%9B%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D1%8B%D0%B5%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B/%D0%9B%D0%B0%D0%B1%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%207%20-%20Unit%20%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5.docx
 
     }
 
